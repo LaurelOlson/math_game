ca227a36d0be9ebee47601b00be5dd174ea86577
LaurelOlson <laurel.olson@outlook.com>
2016-02-08 10:51:59 -0800

    Refactored play method into multiple methods

--- Game.rb
+++ Game.rb
@@ -6,7 +6,7 @@ class Game
     @games_played = 0
   end
 
-  # Checks the lives of player[n] to determine who won the game or if there was a tie.
+  # Checks the lives of player[n] to determine who won the game or if there waas a tie.
   # Displays the winner of the most recent game as well as the total scores of each player.
   def results(players)
     potential_winners = players.find_all { |player| player.lives != 0 }.sort { |x, y| y.score <=> x.score }
@@ -31,14 +31,17 @@ class Game
   end
 
   # Checks the lives of all players to determine if the game is over or if another round of questions should be displayed. 
-  # While the lives of all players is not zero, creates a new Question instance and Round instance for each player, then plays a round. 
+  def game_over?
+    lives = lives = players.collect { |player| player.lives != 0 }
+    lives.include? false
+  end
+
+  # Until game_over? is true, creates a new Question instance and Round instance for each player, then plays a round. 
   # Displays results.
   # Updates games_played.
-  # If less than 5 rounds have been played, asks if the player(s) want(s) to play again and responds accordingly.
-  # If 5 rounds have been played, tells the player(s) to take a break and exits the program.
+  # Calls play_again to determine if the game can/should be played again.
   def play(game, players, games_played)
-    lives = players.collect { |player| player.lives != 0 }
-    while lives.exclude? false
+    until game_over?
       players.each do |player| 
         question = Question.new(1, 20)
         Round.new(player, question).play(player, question)
@@ -47,22 +50,36 @@ class Game
     end
 
     results(players)
-
     games_played += 1
+    play_again(game, players, games_played)
+  end
 
-    if games_played < 5
+  # Checks if player can play again (if less than max number of games has been played)
+  def can_play_again?(games_played)
+    games_played < 5
+  end
+
+  # If can_play_again? is true, asks if the player(s) want to play again.
+  # If can_play_again? is false, tells the player(s) to take a break and exits the program.
+  def wants_to_play_again?(games_played)
+    if can_play_again?(games_played)
       puts "Do you want to play again? (yes or no)"
-      play_again = gets.chomp.to_s
+      play_again = gets.chomp
+      play_again == 'yes'
     else
       puts "Time to take a break!"
     end
-    if play_again == 'yes'
+  end
+
+  # If the player wants to play again, resets scores and lives of each player and then calls game.play to initiate a new game.
+  def play_again(game, players, games_played)
+    if wants_to_play_again?(games_played)
       players.each do |player|
         player.reset_lives
         player.reset_score
       end
       game.play(game, players, games_played)
-    else
     end
   end
+
 end
